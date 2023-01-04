321d2921c7d5f6d93e2e48cabb2a3bca88208e65
Nate Schaffner <thx1766@gmail.com>
2023-01-03 21:13:55 -0500

    Initial Commit

---
+++ wedding/weddingApp.swift
@@ -0,0 +1,17 @@
+//
+//  weddingApp.swift
+//  wedding
+//
+//  Created by Nate M1 on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct weddingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
